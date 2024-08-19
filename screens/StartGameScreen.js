a5c843fcbf8853288c1b4f721f5576e791e24e51
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-19 05:39:25 -0700

    add screen component

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from "react-native";
+
+const StaraGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      Buttons
+    </View>
+  );
+};
+
+export default StaraGameScreen;
