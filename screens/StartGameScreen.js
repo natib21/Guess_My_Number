5754913cee45d649cf42905d43829251025a1c4e
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-19 07:11:41 -0700

    improving the buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -11,8 +11,14 @@ const StaraGameScreen = () => {
         autoCapitalize="none"
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
@@ -21,6 +27,8 @@ export default StaraGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
     marginHorizontal: 24,
     marginTop: 100,
@@ -42,5 +50,12 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
