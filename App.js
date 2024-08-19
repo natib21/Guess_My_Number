8953d9494f69dbb5ec240af38979993788e96ca0
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-19 07:20:29 -0700

    Coloring the Components

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StaraGameScreen from "./screens/StartGameScreen";
 export default function App() {
-  return <StaraGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <StaraGameScreen />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: "#ddb52f",
+  },
+});
