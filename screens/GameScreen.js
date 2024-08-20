9fdc6d0ed200e41b64e525bfdd9f6045c2190c21
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-20 06:48:13 -0700

    Start work on Game Screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,23 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const GameScreen = () => {
-  return <Text>Game Screnn! </Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or Lower</Text>
+        {/* + - */}
+      </View>
+      {/*   <View>LOG ROUNDS</View> */}
+    </View>
+  );
 };
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
