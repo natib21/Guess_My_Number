381493723996b8a78226949ddcb6a08aa66c890b
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-21 07:23:11 -0700

    Adding foreground cimage

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,6 +1,42 @@
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Image } from "react-native";
+import Title from "../components/ui/Title";
 
 const GameOverScreen = () => {
-  return <Text>Game Over</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/image/success.png")}
+        />
+      </View>
+      <View>
+        <Text>Your Phone Needed X rounds to guess the number y.</Text>
+      </View>
+    </View>
+  );
 };
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
