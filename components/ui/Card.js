54760879098482f87f82d04a13597abd17450bd3
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2024-08-29 07:00:58 -0700

    Dimentions API

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,16 +1,17 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 const Card = ({ children }) => {
   return <View style={styles.card}>{children}</View>;
 };
 export default Card;
 
+const deviceWidth = Dimensions.get("window").width;
 const styles = StyleSheet.create({
   card: {
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
     marginHorizontal: 24,
-    marginTop: 100,
+    marginTop: deviceWidth < 380 ? 24 : 36,
     backgroundColor: "#3b021f",
     borderRadius: 8,
     elevation: 4,
